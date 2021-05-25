44e7dad2d054cd7762717607f4fc142948cd0bc7
mikejguo290 <mikejguo290@gmail.com>
2021-05-25 14:15:50 +0100

    filled folder with App.css, HTML template for App.js and a background image.

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class App extends React.Component {
+
+render (){
+    return (
+        <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
+            <!-- Add a SearchBar component -->
+            <div class="App-playlist">
+            <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component -->
+            </div>
+        </div>
+        </div>
+        )
+    }
+}
