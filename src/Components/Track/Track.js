a1e2d6f950f17bd6bd01a9d442e65c0814a33f99
mikejguo290 <mikejguo290@gmail.com>
2021-05-25 18:17:04 +0100

    Added button which will either display + or - to Track.

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Track.css'
 export class Track extends React.Component{
+    renderAction(){
+        let action = isRemoval? '-' : '+';
+        return (
+            <button className="Track-action">{action}</button>
+        );
+    }
     render(){
         return (
             <div class="Track">
-                <div class="trackInformation">
+                <div class="Track-information">
                     <h3><!-- track name will go here --></h3>
                     <p><!-- track artist will go here--> | <!-- track album will go here --></p>
                     </div>
-                <button class="trackAction"><!-- + or - will go here --></button>
+                <button class="Track-action">{this.renderAction}</button>
             </div>
         )
     }
