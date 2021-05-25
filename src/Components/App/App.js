b17e4b7d099d9ac97d25a7ef8e3d7c6d174bcc98
mikejguo290 <mikejguo290@gmail.com>
2021-05-25 14:27:29 +0100

    Adapted HTML to JSX syntax.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,14 +5,14 @@ export class App extends React.Component {
 render (){
     return (
         <div>
-        <h1>Ja<span class="highlight">mmm</span>ing</h1>
-        <div class="App">
-            <!-- Add a SearchBar component -->
-            <div class="App-playlist">
-            <!-- Add a SearchResults component -->
-            <!-- Add a Playlist component -->
+            <h1>Ja<span class="highlight">mmm</span>ing</h1>
+            <div class="App">
+                <!-- Add a SearchBar component -->
+                <div class="App-playlist">
+                <!-- Add a SearchResults component -->
+                <!-- Add a Playlist component -->
+                </div>
             </div>
-        </div>
         </div>
         )
     }
