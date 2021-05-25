3413223c5713afde369557be2284940e69216e75
mikejguo290 <mikejguo290@gmail.com>
2021-05-25 18:20:34 +0100

    changed 'class' in HTML to 'className' for JSX.

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -9,12 +9,12 @@ export class Track extends React.Component{
     }
     render(){
         return (
-            <div class="Track">
-                <div class="Track-information">
+            <div className="Track">
+                <div className="Track-information">
                     <h3><!-- track name will go here --></h3>
                     <p><!-- track artist will go here--> | <!-- track album will go here --></p>
                     </div>
-                <button class="Track-action">{this.renderAction}</button>
+                <button className="Track-action">{this.renderAction}</button>
             </div>
         )
     }
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -5,7 +5,7 @@ import {Track} from '../Track/Track'
 export class TrackList extends React.Component {
     render(){
         return (
-            <div class="TrackList">
+            <div className="TrackList">
                 <Track />
                 <!-- You will add a map method that renders a set of Track components  -->
                 <h2>Stand in track values</h2>
