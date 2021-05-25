5a9edaacbac9a300f19ae2e6373c44b3cf12a66d
mikejguo290 <mikejguo290@gmail.com>
2021-05-25 15:37:31 +0100

    Added HTML and CSS files for SearchBar, SearchResult and Playlist.

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {TrackList} from '../TrackList/TrackList';
+import './Playlist.css'
+
+class Playlist extends React.Component {
+    render(){
+        return (
+            <div class="Playlist">
+                <input value="New Playlist"/>
+                <!-- Add a TrackList component -->
+                <button class="Playlist-save">SAVE TO SPOTIFY</button>
+            </div>
+        )
+    }
+}
