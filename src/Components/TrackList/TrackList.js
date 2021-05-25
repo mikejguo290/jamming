614d1d65841a4b060f496f932d48bbab7f58a050
mikejguo290 <mikejguo290@gmail.com>
2021-05-25 17:25:46 +0100

    imported and added Track component to TrackList.

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './TrackList.css'
+import {Track} from '../Track/Track'
 
 export class TrackList extends React.Component {
     render(){
         return (
             <div class="TrackList">
+                <Track />
                 <!-- You will add a map method that renders a set of Track components  -->
                 <h2>Stand in track values</h2>
                 <ol>
