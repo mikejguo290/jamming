5b61ec8e4a3fbe4c4775dde9b894bd26c22c769e
mikejguo290 <mikejguo290@gmail.com>
2021-05-25 17:00:32 +0100

    export Playlist and SearchResult

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {TrackList} from '../TrackList/TrackList';
 import './Playlist.css'
 
-class Playlist extends React.Component {
+export class Playlist extends React.Component {
     render(){
         return (
             <div class="Playlist">
-                <input value="New Playlist"/>
+                <input defaultValue={'New Playlist'}/>
                 <!-- Add a TrackList component -->
                 <button class="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
